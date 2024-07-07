0bedfebbfd98a3f1ab49512c36d16c3a07aa2121
ruesga-99 <jruesga411@gmail.com>
2024-07-06 23:22:27 -0600

    Updated newtons_finite_differences.py, added functions interpolate and factorial which are used to calculate the polynomial result when evaluated

--- newtons_finite_differences.py
+++ newtons_finite_differences.py
@@ -36,7 +36,7 @@ def backward_finite_differences (tab_func, x):
     n = len(x_val)
 
     h = y_val[1] - y_val[0]
-    s = (x - x_val[0]) / h
+    s = (x - x_val[-1]) / h
 
     table = [[0 for _ in range(n)] for _ in range(n)]
 
@@ -48,6 +48,37 @@ def backward_finite_differences (tab_func, x):
             table[i][j] = (table[i + 1][j - 1] - table[i][j - 1])
 
     return table, x_val, False
+
+def interpolate(x, x_val, table, type):
+    n = len(x_val)
+    
+    if type:  # Forward interpolation
+        h = x_val[1] - x_val[0]
+        s = (x - x_val[0]) / h
+        result = table[0][0]
+        product_term = 1
+        
+        for i in range(1, n):
+            product_term *= (s - (i - 1))
+            result += (product_term * table[0][i]) / factorial(i)
+    
+    else:  # Backward interpolation
+        h = x_val[1] - x_val[0]
+        s = (x - x_val[-1]) / h
+        result = table[-1][0]
+        product_term = 1
+        
+        for i in range(1, n):
+            product_term *= (s + (i - 1))
+            result += (product_term * table[-i-1][i]) / factorial(i)
+    
+    return result
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
 def print_table(table, x_val, type):
     n = len(x_val)
@@ -84,11 +115,15 @@ def main ():
 
     print("\n\033[1mForward Finite Differences\033[0m \n")
     print_table(table, x_val, type)
+    forward_result = interpolate(x, x_val, table, type)
+    print("\nf(x) ≈ {:.5f} at x = {} \n".format(forward_result, x))
 
     table, x_val, type = backward_finite_differences(tab_func, x)
 
     print("\n\033[1mBackward Finite Differences\033[0m \n")
     print_table(table, x_val, type)
+    backward_result = interpolate(x, x_val, table, type)
+    print("\nf(x) ≈ {:.5f} at x = {} \n".format(backward_result, x))
     
     return
 
