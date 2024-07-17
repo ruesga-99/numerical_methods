e3f4c2f9179511bfa38dea73f424e547ab2b8ab8
ruesga-99 <jruesga411@gmail.com>
2024-07-16 20:37:11 -0600

    Updated closed_newton_cotes.py added trapezium method function

--- closed_newton_cotes.py
+++ closed_newton_cotes.py
@@ -12,24 +12,32 @@ def tabular_function (n):
     return tab_func
 
 def calculate_step (n, tab_func):
-    x_values = tab_func.keys()
+    x_values = sorted(tab_func.keys())
+    h = (x_values[-1] - x_values[0]) / (len(x_values) - 1)
 
-    h = (x_values[-1] - x_values[0]) / n
+    return h
 
     return h
 
 def trapezium_method (tab_func, h):
-    x_values = tab_func.keys()
-    y_values = tab_func.values()
+    x_values = sorted(tab_func.keys())
 
-    return 
+    n = len(x_values)
+    integral = 0.5 * (tab_func[x_values[0]] + tab_func[x_values[-1]])
+    
+    for i in range(1, n - 1):
+        integral += tab_func[x_values[i]]
+    
+    integral *= h
+    return integral
 
 def main ():
     n = int(input("Enter the number of known points: "))
     
     tab_func = tabular_function(n)
-
     h = calculate_step(n, tab_func)
+    result = trapezium_method(tab_func, h)
 
+    print("\nThe approximate integral using the trapezium method is: {:.5f} \n".format(result))
 
 main()
